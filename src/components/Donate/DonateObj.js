3ddd8923fe66f9dfb600dcb71e3e8633d1e8437b
Tristanoshier <tsoshier@bsu.edu>
2020-06-01 20:08:26 -0400

    create donation object

--- src/components/Donate/DonateObj.js
+++ src/components/Donate/DonateObj.js
@@ -1,6 +1,45 @@
 
 let donateLinks = {
-    
+    georgeFloydMem: {
+        title : 'Official George Floyd Memorial Fund',
+        desc : 'This fund is established to cover funeral and burial expenses, mental and grief counseling, lodging and travel for all court proceedings, and to assist our family in the days to come as we continue to seek justice for George.  A portion of these funds will also go to the Estate of George Floyd for the benefit and care of his children and their educational fund.',
+        link : 'https://www.gofundme.com/f/georgefloyd?utm_source=customer&utm_medium=copy_link-tip&utm_campaign=p_cp+share-sheet'
+    },
+    indyBailFund: {
+        title : 'The Bail Project',
+        desc : "Join our 8,000+ donors who are helping us post bail for people who can't afford it, reuniting families and restoring the presumption of innocence – one person at a time.",
+        link: 'https://secure.givelively.org/donate/the-bail-project'
+    },
+    nationalBailFund: {
+        title : 'National Bail Fund Network',
+        desc : 'The National Bail Fund Network is made up of over sixty community bail and bond funds across the country.',
+        link: 'https://www.communityjusticeexchange.org/nbfn-directory'
+    },
+    campaignZero: {
+        title : 'Campaign Zero',
+        desc : 'Online platform and organization that utilizes research-based policy solutions to end police brutality in America.',
+        link: 'https://www.paypal.com/donate/?token=0ANKtvOFGhM_qQ0fe8NI9tAAS-58vzb8nNw_3LIRpUfdTnVNiD-mXh7ufaKnO0aH3ubDn0&country.x=US&locale.x=US'
+    },
+    minnesotaFreeFund: {
+        title : 'Minnesota Freedom Fund',
+        desc : 'Community-based nonprofit that pays criminal bail and immigration bonds for individuals who have been arrested while protesting police brutality.',
+        link: 'https://minnesotafreedomfund.org/donate'
+    },
+    blackVisionsCollective: {
+        title : 'Black Visions Collective',
+        desc : 'A black, trans, and queer-led organization that is committed to dismantling systems of oppression and violence, and shifting the public narrative to create transformative, long-term change. ',
+        link: 'https://secure.everyaction.com/4omQDAR0oUiUagTu0EG-Ig2'
+    },
+    reclaimBlock: {
+        title : 'Reclaim the Block',
+        desc : 'Coalation that advocates for and invests in community-led safety initiatives in Minneapolis neighborhoods.',
+        link: 'https://secure.everyaction.com/zae4prEeKESHBy0MKXTIcQ2'
+    },
+    unicornRiot: {
+        title : 'Unicorn Riot',
+        desc : 'Non-profit organization that is dedicated to exposing root causes of dynamic social and environmental issues.',
+        link: 'https://unicornriot.ninja/donate/'
+    },
 }
 
 export default donateLinks;
